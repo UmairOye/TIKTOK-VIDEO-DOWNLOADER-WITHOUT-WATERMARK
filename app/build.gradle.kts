524d98775800f35f6ea10b7631e6182420e515ff
umair <askedumairbashir@gmail.com>
2024-09-28 11:29:25 +0500

    libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,20 +50,20 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     //SDP TO SHOW SAME VIEW GROUP SIZES ON ALL SCREENS
     implementation ("com.intuit.sdp:sdp-android:1.1.0")
 
     //NAVIGATION TO NAVIGATION FROM ONE DESTINATION TO OTHER
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.1")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.8.1")
 
     //SSP FOR TO SET SAME TEXT SIZE ON ALL SCREEN SIZES
     implementation ("com.intuit.ssp:ssp-android:1.1.0")
